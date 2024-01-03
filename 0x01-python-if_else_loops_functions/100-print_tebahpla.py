829ddd42f3fe6fbdd9d39c74c237aac058fdae23
MrWavyEnt <chimacollinsekoma@gmail.com>
2024-01-03 23:52:14 +0200

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-i in range (ord('z'), ord('a') -1, -2):
-    print("{}{}".format(chr(i), char(i - 33)), end='')
+i in range(ord('z'), ord('a') -1, -2):
+    print("{}{}".format(chr(i), chr(i - 33)), end='')
