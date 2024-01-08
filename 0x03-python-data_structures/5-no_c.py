b0766c20115e869627ad1d57d2b850e8c039deef
MrWavyEnt <chimacollinsekoma@gmail.com>
2024-01-08 13:35:38 +0200

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    trans_table = {oed('c') None, ord('c'): None}
+    trans_table = {ord('c') None, ord('c'): None}
     return(my_string.translate(trans_table))
