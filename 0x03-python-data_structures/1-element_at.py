7df5cce35635748c7b06932393942901fb4ef3c8
MrWavyEnt <chimacollinsekoma@gmail.com>
2024-01-08 12:03:31 +0200

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < O:
-        idx += len(my_List)
-    if idx >= len(my_list):
+        return(None)
+    elif idx >= len(my_list):
         return (None)
     return my_list[idx]
