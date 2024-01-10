1588c710419a4d347f7598a670c28316ff1a59d1
MrWavyEnt <chimacollinsekoma@gmail.com>
2024-01-10 22:37:57 +0200

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,17 @@
-def main():
-   total = 0
-   for arg in sys.argv[1:]:
-       total += int(arg)
-   print(total)
+#!/usr/bin/python3
+import sys
+
+def add_arguments(arguments):
+    return sum(int(arg) for arg in arguments)
 
 if __name__ == "__main__":
-   main()
+    # Extract command line arguments excluding the script name
+    arguments = sys.argv[1:]
+
+    # Check if there are any arguments
+    if arguments:
+        # Calculate the sum and print the result
+        result = add_arguments(arguments)
+        print(result)
+    else:
+        print("No arguments provided.")
