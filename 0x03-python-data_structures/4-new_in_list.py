d61c0907d51b085162d5e5a772132e16b47dbeab
MrWavyEnt <chimacollinsekoma@gmail.com>
2024-01-17 12:15:00 +0200

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return (my_list)
-    elif idx >= len(my_list)
-    return (my_list)
+    if idx < 0 or idx > len(my_list):
+        raise ValuError("Invalid index")
     new_list = list(my_list)
-    new_list[idx] = element
+    new_list.insert(idx, element)
     return (new_list)
