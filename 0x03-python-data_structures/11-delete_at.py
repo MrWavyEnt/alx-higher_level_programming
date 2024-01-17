82e486077ad77581534c7cbaf4f8c94c99076746
MrWavyEnt <chimacollinsekoma@gmail.com>
2024-01-17 12:23:26 +0200

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,5 +3,4 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return (my_list)
     else:
-        my_list.pop(idx)
-        return (my_list)
+        return (my_list[:idx] + my_list[idx+1:])
